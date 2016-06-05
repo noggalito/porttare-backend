e3347628fc79d431e70f3fff23adc1707d096df7
Mario Andrés Correa <a@macool.me>
2016-06-05 07:17:16 -0500

    bootstrap devise_token_auth on User model

--- README.md
+++ README.md
@@ -8,3 +8,24 @@
 
 - postgresql 9+
 - ruby 2.3.0
+- bundler
+
+### running app
+
+- bundle app
+
+  ```
+$ bundle
+  ```
+
+- create PostgreSQL DB & migrate
+
+  ```
+$ bundle exec rake db:create db:migrate
+  ```
+
+- launch rails server
+
+  ```
+$ bundle exec rails server
+  ```
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
