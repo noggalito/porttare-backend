ef930edeac1e4129816b90c5b66108297fa3ecbd
Mario Andrés Correa <a@macool.me>
2016-09-19 19:44:18 -0500

    meh

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,10 +1,8 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   Bullet.enable = true
   Bullet.rails_logger = true
-  Bullet.bullet_logger = true
 end
 
-if Rails.env.test?
-  Bullet.enable = true
-  Bullet.raise = true
+if Rails.env.development?
+  Bullet.bullet_logger = true
 end
