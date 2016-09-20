6a3f1c3cba46b8f5dcd0dcba92ddf85ddc276a4d
Mario Andrés Correa <a@macool.me>
2016-09-19 19:22:36 -0500

    do not attempt to log bullet in test env

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,9 +1,10 @@
-if Rails.env.development? || Rails.env.test?
+if Rails.env.development?
   Bullet.enable = true
   Bullet.rails_logger = true
   Bullet.bullet_logger = true
 end
 
 if Rails.env.test?
+  Bullet.enable = true
   Bullet.raise = true
 end
