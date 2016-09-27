8a1abbdd377029eff599a127e6d072b662cdf3c9
Fabricio Flores <fabrifloresg@gmail.com>
2016-09-27 21:52:14 +0200

    Add route and controller for dispatchers

--- app/controllers/api/provider/dispatchers_controller.rb
+++ app/controllers/api/provider/dispatchers_controller.rb
@@ -3,74 +3,75 @@ module Api
     class DispatchersController < BaseController
       resource_description do
         name "Provider::Dispathchers"
-        short "provider's clients"
+        short "provider's dispatchers"
       end
 
       before_action :authenticate_api_auth_user!
-      before_action :find_provider_client,
+      before_action :find_provider_dispatcher,
                     only: [:update, :destroy]
 
       api :GET,
-          "/provider/clients",
-          "Lists provider's clients"
+          "/provider/dispatchers",
+          "Lists provider's dispatchers"
       example %q{{
-  "provider_clients":[
-    {
-      "id":1,
-      "notas":"jerarquía analizada Diverso",
-      "ruc":"961770900-7",
-      "nombres":"Urías S.A.",
-      "direccion":"Subida Clemente Zapata, 9 Esc. 773",
-      "telefono":"972 299 498",
-      "email":"karelle@luettgenlueilwitz.name",
-      "created_at":"2016-09-24T09:15:53.617-05:00"
-    }
-  ]
-}}
+        "provider_dispatchers":[
+          {
+            "id":1,
+            "email":"karelle@luettgenlueilwitz.name",
+            "ruc":"961770900-7",
+            "nombres":"Perico de los Palotes",
+            "email":"karelle@luettgenlueilwitz.name",
+            "local": {
+              nombre: "sucursal 666",
+              id: 1
+            }
+          }
+        ]
+      }}
       def index
-        authorize ProviderClient
-        @provider_clients = provider_scope
+        authorize ProviderDispatcher
+        @provider_dispatchers = provider_scope
       end
 
-      def_param_group :provider_client do
-        param :notas, String
-        param :ruc, String, required: true
-        param :nombres, String, required: true
-        param :direccion, String, required: true
-        param :telefono, String, required: true
+      def_param_group :provider_dispatcher do
         param :email, String, required: true
+        param :ruc, String, required: true
+        param :provider_office_id, Integer, required: true
       end
 
       api :POST,
-          "/provider/clients",
-          "Create a provider client"
-      param_group :provider_client
+          "/provider/dispatchers",
+          "Create a provider dispatcher"
+      param_group :provider_dispatcher
       def create
-        authorize ProviderClient
-        @provider_client =
+        authorize ProviderDispatcher
+        user = User.find_by_email provider_client_params.email
+        @provider_dispatcher =
           current_api_auth_user
             .provider_profile
-            .provider_clients.new(provider_client_params)
-        if @provider_client.save
-          render :client, status: :created
+            .provider_dispatchers.new(provider_dispatcher_params)
+        @provider_dispatcher.user = user if user
+        if @provider_dispatcher.save
+          render :dispatcher, status: :created
         else
-          @errors = @provider_client.errors
+          @errors = @provider_dispatcher.errors
           render "api/shared/resource_error",
                  status: :unprocessable_entity
         end
       end
 
       api :PUT,
-          "/provider/clients/:id",
-          "Update a provider's client"
+          "/provider/dispatchers/:id",
+          "Update a provider's dispatcher"
       param :id,
             Integer,
             required: true,
-            desc: "Provider client's id"
-      param_group :provider_client
+            desc: "Provider dispatcher's id"
+      param_group :provider_dispatcher
       def update
-        authorize @provider_client
-        if @provider_client.update_attributes(provider_client_params)
+        authorize @provider_dispatcher
+        # TODO do we need to update the user_id and provider_office?
+        if @provider_dispatcher.update_attributes(provider_client_params)
           render :client, status: :accepted
         else
           @errors = @provider_client.errors
@@ -80,16 +81,16 @@ module Api
       end
 
       api :DELETE,
-          "/provider/clients/:id",
-          "Delete a provider's client"
+          "/provider/dispatchers/:id",
+          "Delete a provider's dispatcher"
       desc "a soft delete is performed"
       param :id,
             Integer,
             required: true,
-            desc: "Provider client's id"
+            desc: "Provider dispatcher's id"
       def destroy
-        authorize @provider_client
-        @provider_client.soft_destroy
+        authorize @provider_dispatcher
+        @provider_dispatcher.soft_destroy
         head :no_content
       end
 
@@ -97,16 +98,16 @@ module Api
 
       def provider_client_params
         params.permit(
-          *policy(ProviderClient).permitted_attributes
+          *policy(ProviderDispatcher).permitted_attributes
         )
       end
 
-      def find_provider_client
-        @provider_client = provider_scope.find(params[:id])
+      def find_provider_dispatcher
+        @provider_dispatcher = provider_scope.find(params[:id])
       end
 
       def provider_scope
-        policy_scope(ProviderClient)
+        policy_scope(ProviderDispatcher)
       end
     end
   end
---
+++ app/policies/provider_dispatcher_policy.rb
@@ -0,0 +1,39 @@
+class ProviderClientPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(
+        provider_profile_id: user.provider_profile.id
+      )
+    end
+  end
+
+  def index?
+    is_provider?
+  end
+
+  def create?
+    is_provider?
+  end
+
+  def update?
+    is_provider?
+  end
+
+  def destroy?
+    is_provider?
+  end
+
+  def permitted_attributes
+    [
+      :email,
+      :ruc,
+      :provider_office_id
+    ]
+  end
+
+  private
+
+  def is_provider?
+    user.provider_profile.present?
+  end
+end
