eb0da873b629295a8c51a61342279f4ecefc930f
Mario Andrés <a@macool.me>
2016-09-29 11:19:40 -0500

    revise ProviderDispatcher

--- db/migrate/20160926181537_create_provider_dispatchers.rb
+++ db/migrate/20160926181537_create_provider_dispatchers.rb
@@ -1,8 +1,8 @@
 class CreateProviderDispatchers < ActiveRecord::Migration
   def change
     create_table :provider_dispatchers do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :provider_office, index: true, foreign_key: true
+      t.references :provider_profile, null: false, index: true, foreign_key: true
+      t.references :provider_office, null: false, index: true, foreign_key: true
       t.string :email
 
       t.timestamps null: false
